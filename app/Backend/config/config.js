3c4e35ebb9359b68ced7c5747f6d9a345c65bef9
rahma <fijriatirahmaturrizqi@gmail.com>
2025-08-02 19:23:29 +0700

    fix: setup config migration

---
+++ app/Backend/config/config.js
@@ -0,0 +1,25 @@
+require('dotenv').config(); // this is important!
+const process = require('process');
+module.exports = {
+  development: {
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+  },
+  test: {
+    username: 'root',
+    password: null,
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
+  production: {
+    username: 'root',
+    password: null,
+    database: 'database_production',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
+};
